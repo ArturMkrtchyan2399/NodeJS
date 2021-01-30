cb63f12f7cd5d8b71e23ea08f4cdc2e4001b09e2
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2021-01-30 17:01:20 +0400

    update

--- NodeJS/Practic/promiseAll.js
+++ NodeJS/Practic/promiseAll.js
@@ -1,1 +1,51 @@
+function promiseAll(array) {
+    return new Promise((resolve,reject) =>{
+        let result = []
+        let completed = 0
+        for(let i = 0;i < array.length;i++){
+            array[i].then((answer) => {
+                result[i] = answer
+                completed+=1
+                if(completed === array.length){
+                    resolve(result)
+                }
+            }).catch((err) => {
+                reject(err)
+            })
+        }
+
+    })
+}
+
+const promise1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise 1")
+    }, 1000);
+})
+const promise2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise 2")
+    }, 1000);
+})
+const promise3 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise 3")
+    }, 1000);
+})
+const promise4 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise 4")
+    }, 1000);
+})
+promiseAll([promise1,promise2,promise3,promise4])
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+
+
 
