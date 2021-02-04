6b3fa33f40c1ddb3b8bfd941be46810261d9d299
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2021-02-04 19:41:50 +0400

    add homework

--- JS/Practic/promiseRace.js
+++ JS/Practic/promiseRace.js
@@ -1,11 +1,19 @@
 function promiseRace(array) {
     return new Promise((resolve, reject) => {
+        let isRejected = false;
+        let isResolved = false;
         for (let i = 0; i < array.length; i++) {
             array[i].then((answer) => {
-                resolve(answer)
-
+                if (!isResolved) {
+                    isResolved = true;
+                    resolve(answer)
+                }
             }).catch((err) => {
-                reject(err)
+                if (!isRejected) {
+                    isRejected = true
+                    reject(err)
+                }
+
             })
         }
 
